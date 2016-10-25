5c80410ce706ee8261d17ae0aeb35039fd8a8ffb
Andrzej Górski <andrzejgorski@supermond.com>
2016-10-26 00:30:06 +0200

    Simple fix recreate db script

--- recreate_db.sh
+++ recreate_db.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-cat drop_tables.sql | sqlite3 baza_dblp.sqlite3; cat create_database_schema.sql | sqlite3 bazadblp.sqlite3
+cat drop_tables.sql | sqlite3 baza_dblp.sqlite3; cat create_database_schema.sql | sqlite3 baza_dblp.sqlite3
